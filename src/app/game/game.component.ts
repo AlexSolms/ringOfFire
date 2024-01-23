1ef05af834b6b1a9e313ecf72f00319e1d3e27e1
Alex Solms <duaedin@web.de>
2024-01-23 20:33:35 +0100

    pop and push works for the drwan card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,22 +10,31 @@ import { Game } from '../../models/game';
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
- 
+
   pickCardAnimation = false;
   game: Game;
+  currentCard: string | undefined = '';
 
   constructor() {
     this.game = new Game();
-    console.log(this.game);
-    //this.newGame();
+    console.log(this.game.stack);
+
   }
 
-/*   newGame() {
-    this.game = new Game();
-    console.log(this.game);
-  } */
+/**
+ * this function pops and pushes cards from one array to another and set the playCardAnimationflag
+ * DRAWNCARD is needed because Typescript need the check if it is undefiend
+ */
   takeCard() {
-    this.pickCardAnimation = true;
+    // Prüfen, ob der Stack nicht leer ist, bevor Sie eine Karte nehmen
+    if (!this.pickCardAnimation) {
+      const DRAWNCARD: string | undefined = this.currentCard = this.game.stack.pop();
+      if (DRAWNCARD !== undefined) {
+        this.pickCardAnimation = true;
+        setTimeout(() => this.game.playedCards.push(DRAWNCARD), 1000);
+      }
+      setTimeout(() => this.pickCardAnimation = false, 1500);
+    }
   }
 
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -7,11 +7,25 @@ export class Game{
     constructor(){
         for (let i = 1; i < 14; i++) {
             this.stack.push('spade_' + i);
-            this.stack.push('heart_' + i);
+            this.stack.push('hearts_' + i);
             this.stack.push('clubs_' + i);
             this.stack.push('diamonds_' + i);
         }
-        
+        shuffle(this.stack);
     }
 }
 
+/**
+ * this Function shuffles the array
+ * @param array - array which should be schuffled
+ * @returns shuffled array
+ */ 
+const shuffle = (array: string[]) => { 
+    for (let i = array.length - 1; i > 0; i--) { 
+      const j = Math.floor(Math.random() * (i + 1)); 
+      [array[i], array[j]] = [array[j], array[i]]; 
+    } 
+    return array; 
+  }; 
+    
+  
