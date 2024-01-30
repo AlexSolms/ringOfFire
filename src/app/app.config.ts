b1f833e77616ebe9b5642a730ca36f0da51724d2
Alex Solms <duaedin@web.de>
2024-01-30 11:25:58 +0100

    testing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"ringoffire-e357a","appId":"1:696382858209:web:30df4f088dd7655407e8a0","storageBucket":"ringoffire-e357a.appspot.com","apiKey":"AIzaSyAT7TdIwnLQ6qSh0jXXe7dXCqKvZz4bapk","authDomain":"ringoffire-e357a.firebaseapp.com","messagingSenderId":"696382858209"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [provideRouter(routes), provideAnimations(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"ringoffire-e357a","appId":"1:696382858209:web:30df4f088dd7655407e8a0","storageBucket":"ringoffire-e357a.appspot.com","apiKey":"AIzaSyAT7TdIwnLQ6qSh0jXXe7dXCqKvZz4bapk","authDomain":"ringoffire-e357a.firebaseapp.com","messagingSenderId":"696382858209"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"ringoffire-e357a","appId":"1:696382858209:web:30df4f088dd7655407e8a0","storageBucket":"ringoffire-e357a.appspot.com","apiKey":"AIzaSyAT7TdIwnLQ6qSh0jXXe7dXCqKvZz4bapk","authDomain":"ringoffire-e357a.firebaseapp.com","messagingSenderId":"696382858209"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
